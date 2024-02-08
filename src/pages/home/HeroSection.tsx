0bfc2c22c60cf52495277f7d4af482ed60214e56
muhammadali246397 <muhammadali246397@gmail.com>
2024-02-09 01:34:35 +0600

    make animation for hero section

--- src/pages/home/HeroSection.tsx
+++ src/pages/home/HeroSection.tsx
@@ -1,21 +1,40 @@
 import Container from "@/lib/Container";
 import img from "./../../assets/image/macbook-exposed.png"
 import { Button } from "@/components/ui/button";
-
-
+import { motion } from "framer-motion"
 const HeroSection = () => {
+  const intro = {
+    hidden:{opacity:0},
+    visible:{
+      opacity:1,
+      transition:{duration:0.5, staggerChildren: 0.25,delayChildren:1}
+    }
+  }
+  const introChildren={
+    hidden:{opacity:0,y:-200},
+    visible:{opacity:1, y:0,transition:{duration:0.7,type:'spring',bounce:0.4}}
+  }
     return (
         <div>
-            <Container className="grid grid-cols-2 h-screen bg-red-700">
-              <div>
-                <h1 className="text-8xl font-bold">Don't worry,</h1>
-                <h1 className="text-8xl font-bold">We'll fix it.</h1>
-                <p className="">Welcome to <span className="font-semibold">iRepair</span>, your one-stop place for all kinds of <span className="font-semibold">Macbook repairs</span> and diagnostics.</p>
+            <Container className="grid grid-cols-1 lg:grid-cols-2 h-screen justify-between place-content-center">
+              <motion.div
+              variants={intro}
+              initial="hidden"
+              animate="visible"
+              >
+                <motion.h1 variants={introChildren} className="text-5xl lg:text-8xl text-nowrap font-bold">
+                  <span className="text-gray">Don't worry,</span>
+                  <br />
+                  We'll fix it.</motion.h1>
+                
+                <motion.p variants={introChildren} className="max-w-[42ch] mt-10 mb-6">Welcome to <span className="font-semibold">iRepair</span>, your one-stop place for all kinds of <span className="font-semibold">Macbook repairs</span> and diagnostics.</motion.p>
+                <motion.div variants={introChildren}>
                 <Button>Book a service</Button>
+                </motion.div>
 
-              </div>
-              <div>
-                <img src={img} alt="" />
+              </motion.div>
+              <div className=" w-[85%] mt-[10px] mx-auto lg:max-w-full">
+                <img className="-rotate-[34deg] h-[95%] bg-cover object-contain" src={img} alt="" />
               </div>
             </Container>
         </div>
